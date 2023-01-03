d51a21ff34aa534eafaadcdbc1bee85fa8962964
snowmanstudio <105069282+snowmanstudio@users.noreply.github.com>
2023-01-04 03:35:35 +0600

    update gridsome

--- gridsome.config.js
+++ gridsome.config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       use: "gridsome-source-graphql",
       options: {
-        url: "http://freehub.online/graphql",
+        url: "https://freehub.online/graphql",
         typeName: "WPGraphQL",
         fieldName: "wpgraphql",
       },
